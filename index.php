b5dec8695551290339ba9f94a9744f20129e96d1
Nootan Ghimire <nootan.ghimire@gmail.com>
2013-08-13 10:40:47 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Dynamic Javascript Example</title>
+<script type="text/javascript">
+<?php require_once("dynamic_gallery.js.php"); ?>
+</script>
+
+<!-- Or You can use : 
+
+<script type="text/javascript" src="dynamic_gallery.js.php"></script>
+
+-->
+
+</head>
+<body>
+
+<img src="gallery/default_img.jpg" name="slideshow" id="slideshow" alt="Gallery!" />
+
+<!-- Do not Change name and id, It may cause error in javascript file -->
+
+
+</body>
+</html>
